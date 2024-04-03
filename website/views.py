b396bba7e512600355055045ded85e2d26f25a7b
Unnati Patil <unnatipatil911@gmail.com>
2024-04-03 13:07:16 +0530

    please add my branch

--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm, AddRecordForm
 from .models import Record
-
+#i am unnati patil
 
 def home(request):
 	records = Record.objects.all()
